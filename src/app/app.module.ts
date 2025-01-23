4da7f288ae1a9bdd7c06b1acfee98442c85890f2
Univesp-ErickBento <erickbento.univesp@gmail.com>
2025-01-22 21:05:19 -0300

    fazendo o form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,41 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {ChangeDetectionStrategy} from '@angular/core';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+import {MatButtonModule} from '@angular/material/button';
+
+import {Component} from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatDividerModule, 
+    MatIconModule,
+    MatSelectModule,
+    MatInputModule,
+    Component,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export class ButtonOverviewExample {}
+export class FormFieldOverviewExample {}
