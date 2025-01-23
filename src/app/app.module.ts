b741aa4f6ef2fbcafee82005167025fadd941d98
Univesp-ErickBento <erickbento.univesp@gmail.com>
2025-01-23 12:35:07 -0300

    component nav criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,41 +5,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {ChangeDetectionStrategy} from '@angular/core';
-import {MatSelectModule} from '@angular/material/select';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-
-import {MatButtonModule} from '@angular/material/button';
-
-import {Component} from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatDividerModule} from '@angular/material/divider';
-
-
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    
+    NavComponent,
   ],
-
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatFormFieldModule,
     MatButtonModule,
-    MatDividerModule, 
+    MatDividerModule,
     MatIconModule,
     MatSelectModule,
     MatInputModule,
-    Component,
-    
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-export class ButtonOverviewExample {}
-export class FormFieldOverviewExample {}
