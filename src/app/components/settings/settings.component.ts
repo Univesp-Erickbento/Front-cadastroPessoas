ded60d8a370b1ff201c5514142ec0dae419119d9
isaque F <solatudokk@gmail.com>
2025-01-24 20:52:41 -0300

    nao sei o que eu fiz mas eu fiz um negocio aqui

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -5,5 +5,6 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'sapp-settings',
   templateUrl: './settings.component.html',
+  styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent {}
